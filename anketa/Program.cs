10294fdd256e997452034ab07dfd9aa6ee9a0d4d
shram78 <ra00275@gmail.com>
2021-01-09 12:26:39 +0300

    refactoring

--- anketa/Program.cs
+++ anketa/Program.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*Вы задаете вопросы пользователю, по типу "как вас зовут", "какой ваш знак зодиака" и тд,
-после чего, по данным, которые он ввел, формируете небольшой текст о пользователе.
-"Вас зовут Алексей, вам 21 год, вы водолей и работаете на заводе."*/
 
 namespace anketa
 {
@@ -13,20 +10,20 @@ namespace anketa
     {
         static void Main(string[] args)
         {
+            string myName, myTownWasBorn, learningLanguage;
+            int myAge;
             Console.WriteLine("Анкета\n");
             Console.Write("Введи своё имя: ");
-            string myName = Console.ReadLine();
+            myName = Console.ReadLine();
             Console.Write("Сколько тебе лет? ");
-            int myYarsOld = Convert.ToInt32(Console.ReadLine());
+            myAge = Convert.ToInt32(Console.ReadLine());
             Console.Write("В каком городе ты родился? ");
-            string myTownWasBorn = Console.ReadLine();
+            myTownWasBorn = Console.ReadLine();
             Console.Write("Какой язык программирования ты изучаешь? ");
-            string cSharpOfCorse = Console.ReadLine();
-
-            Console.WriteLine($"\nТебя зовут {myName}, тебе {myYarsOld} лет, ты родился в городе {myTownWasBorn}," +
-                              $" ты освоил основы языка {cSharpOfCorse}");
-
-                Console.ReadKey();
+            learningLanguage = Console.ReadLine();
+            Console.WriteLine($"\nТебя зовут {myName}, тебе {myAge} лет, ты родился в городе {myTownWasBorn}," +
+                  $" ты освоил основы языка {learningLanguage}");
+            Console.ReadKey();
         }
     }
 }
